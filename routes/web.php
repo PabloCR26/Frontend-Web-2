69ace51ef1c282fc8e0fd9aae65ed8138b0ac838
PabloCR26 <chavespablo2605@gmail.com>
2026-04-13 20:51:36 -0600

    Creacion del front

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return view('layouts.dashboard');
+})->name('dashboard');
+
+
+
